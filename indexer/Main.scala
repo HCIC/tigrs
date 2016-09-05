6f54a63c828836350b0b0e7c91eaf4781a32f4bf
Felix Dietze <felix@beerleader.net>
2016-09-05 20:19:51 +0200

    create search index in backend and write to json file

--- indexer/Main.scala
+++ indexer/Main.scala
@@ -4,21 +4,44 @@ import scala.scalajs.js
 import scala.scalajs.js.JSApp
 import js.Dynamic.{global => g}
 import js.DynamicImplicits._
+import boopickle.Default._
+import scala.scalajs.js.typedarray.TypedArrayBufferOps._
+import scala.scalajs.js.typedarray._
+import scala.scalajs.js.typedarray.Uint8Array
 
 object TutorialApp extends JSApp {
+  lazy val fs = g.require("fs")
+  lazy val elasticlunr = g.require("elasticlunr")
+
   def main(): Unit = {
-    val elasticlunr = g.require("elasticlunr")
-    println(elasticlunr.version)
+    val index = createIndex(readPublications)
 
-    val fs = g.require("fs")
-    fs.writeFile("/tmp/test", "Hey there!", { (err: js.Dynamic) =>
-      if (err)
-        println(err)
-      else
-        println("The file was saved!")
-    })
+    println("writing index to data/fakall.boo.json...")
+    fs.writeFile("data/fakall.index.json", index.toJSON())
+  }
 
-    val p = Publications(Nil)
-    println(p)
+  def createIndex(pubs: Publications): js.Dynamic = {
+    println("creating search index...")
+    val index = elasticlunr()
+    index.addField("title")
+    index.setRef("recordId")
+    index.saveDocument(false)
+
+    for (pub <- pubs.publications) {
+      index.addDoc(pub.asInstanceOf[js.Any])
+    }
+
+    println(index._fields)
+
+    index
+  }
+
+  lazy val readPublications: Publications = {
+    println("reading data/fakall.boo...")
+    val buf = TypedArrayBuffer.wrap(fs.readFileSync("data/fakall.boo").asInstanceOf[ArrayBuffer])
+
+    println("decoding...")
+    import PublicationPickler._
+    Unpickle[Publications].fromBytes(buf)
   }
 }
