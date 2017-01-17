857581ba9aecc8a3ca14ae785891a9bd7225a34f
Felix Dietze <felix@beerleader.net>
2017-01-17 15:24:54 +0100

    split deploy-data

--- deploy-data.sh
+++ deploy-data.sh
@@ -1,10 +1,4 @@
 #!/bin/bash -e
 
 ./prepare-data.sh
-{
-  echo 'progress'
-  echo '-rm data/*'
-  echo '-mkdir data'
-  echo 'put -R data/*.gz data'
-  echo 'put -R data/*.boo data'
-} | sftp -b - $DEPLOY_USER@$DEPLOY_HOST
+./upload-data.sh
---
+++ upload-data.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -e
+
+{
+  echo 'progress'
+  echo '-rm data/*'
+  echo '-mkdir data'
+  echo 'put -R data/*.gz data'
+  echo 'put -R data/*.boo data'
+} | sftp -b - $DEPLOY_USER@$DEPLOY_HOST
